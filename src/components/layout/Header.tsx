bd21289bfe677684cc4c3053c63216a9babec01e
JUHEEBAEK <bjh9287@naver.com>
2024-08-27 21:50:44 +0900

    Feat: header 컴포넌트로 빼기

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header>
+      <div className='links'>
+        <a href='/home' rel='noreferrer'>
+          Home
+        </a>
+        <a href='/basic' rel='noreferrer'>
+          basic page
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
