3f8d8b63d76d3d932072cffdd428a8dc27f2e7cb
JUHEEBAEK <bjh9287@naver.com>
2024-08-31 02:46:36 +0900

    Feat: Quiz 컴포넌트 생성

--- src/components/quiz/Question.styles.ts
+++ src/components/quiz/Question.styles.ts
@@ -1,37 +1,38 @@
 import styled from 'styled-components';
 
 import { colors } from '@/styles/colors';
+import { TextNoneDrag } from '@/styles/mixins';
 
 export const QuestionContainer = styled.div<{ $backgroundImage: string }>`
-  padding: 2.8rem;
+  padding: 2.4rem;
   height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
+  justify-content: space-between;
   background-image: url(${props => props.$backgroundImage});
   background-size: cover;
   background-position: center;
-  border-radius: 8px; /* 배경 이미지가 네모나지 않도록 둥글게 */
   color: ${colors.white}; /* 텍스트 색상을 흰색으로 설정하여 가독성 향상 */
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* 약간의 그림자를 추가해 입체감 부여 */
 `;
 
-export const QuestionText = styled.h3`
-  font-size: 1.25rem; /* text-lg */
-  font-weight: bold;
-  margin-bottom: 1rem; /* mb-4 */
+export const QuestionBox = styled.div`
+  color: white;
+  border: 2px solid white;
+  padding: 1rem;
+  text-align: center;
 `;
 
-export const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  gap: 0.5rem;
+export const QuestionText = styled.h3`
+  ${TextNoneDrag}
+  font-size: 1.25rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
 `;
 
 export const Button = styled.button`
-  background-color: inherit;
-  border: solid 3px ${colors.white};
-  color: ${colors.white};
+  background-color: #ffffff80;
+  color: ${colors.black};
   padding: 1rem 1.6rem;
   border-radius: 0.375rem;
   font-size: 1.125rem;
@@ -39,7 +40,38 @@ export const Button = styled.button`
   cursor: pointer;
 
   &:hover {
-    background-color: ${colors.primary};
+    background-color: ${colors.primaryOpacity};
     color: ${colors.white};
+  }
+`;
+
+export const SelectContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 12px;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 16px;
+`;
+
+export const OptionText = styled.h4`
+  ${TextNoneDrag}
+  padding: 0.875rem;
+  color: ${colors.white};
+  font-size: 1rem;
+  max-width: 120px;
+  min-height: 100px;
+  vertical-align: middle;
+
+  &:first-child {
+    text-align: left;
+  }
+  &:last-child {
+    text-align: right;
   }
 `;
--- src/components/quiz/Question.tsx
+++ src/components/quiz/Question.tsx
@@ -1,20 +1,53 @@
-import { QuestionContainer, QuestionText, ButtonContainer, Button } from './Question.styles';
+import {
+  QuestionContainer,
+  QuestionBox,
+  QuestionText,
+  SelectContainer,
+  TextContainer,
+  OptionText,
+  Button,
+} from './Question.styles';
 
-import backgroundImg from '@/assets/bg_field.png'; // 배경 이미지 경로
+import backgroundImg from '@/assets/bg-soccer-field.png';
+import CircleSelector from '@/components/quiz/CircleSelector';
+import { useState } from 'react';
 
-type QuestionProps = {
+interface IQuestionProps {
   questionText: string;
-  onAnswer: (answer: boolean) => void;
-};
+  options: { type: string; text: string }[];
+  scale: { min: string; max: string };
+  onAnswer: (scaleValue: { [key: string]: number }) => void;
+}
 
-const Question = ({ questionText, onAnswer }: QuestionProps) => {
+const Question = ({ questionText, scale, options, onAnswer }: IQuestionProps) => {
+  const [selectedValue, setSelectedValue] = useState<number | null>(); // 초기 값은 중앙
+
+  const handleSelect = (value: number) => {
+    setSelectedValue(value);
+  };
+
+  const handleSubmit = () => {
+    if (selectedValue)
+      const scaleValue = {
+        [scale.min]: 100 - selectedValue,
+        [scale.max]: selectedValue,
+      };
+    onAnswer(scaleValue);
+  };
+
   return (
     <QuestionContainer $backgroundImage={backgroundImg}>
-      <QuestionText>{questionText}</QuestionText>
-      <ButtonContainer>
-        <Button onClick={() => onAnswer(true)}>Yes</Button>
-        <Button onClick={() => onAnswer(false)}>No</Button>
-      </ButtonContainer>
+      <QuestionBox>
+        <QuestionText>{questionText}</QuestionText>
+      </QuestionBox>
+      <SelectContainer>
+        <CircleSelector onSelect={handleSelect} />
+        <TextContainer>
+          <OptionText>{options[0].text}</OptionText>
+          <OptionText>{options[1].text}</OptionText>
+        </TextContainer>
+      </SelectContainer>
+      <Button onClick={handleSubmit}>다음</Button>
     </QuestionContainer>
   );
 };
