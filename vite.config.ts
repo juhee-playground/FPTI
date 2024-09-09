6f05e1473be1b328e15102e84470fae88b5ef39f
JUHEEBAEK <bjh9287@naver.com>
2024-09-09 21:12:50 +0900

    Chore: text 압축 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    viteCompression({
+      // 압축 알고리즘 지정, 기본적으로는 'gzip'을 사용
+      algorithm: 'gzip',
+      // 압축된 파일의 확장자를 '.gz'로 설정
+      ext: '.gz',
+    }),
+  ],
   resolve: {
     alias: {
       '@': '/src',
