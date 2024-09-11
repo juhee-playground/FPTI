4ec8b7811686737e0b4937320e7b41acf3b98056
JUHEEBAEK <bjh9287@naver.com>
2024-09-11 16:07:08 +0900

    Fix: header 없애기

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,19 +1,13 @@
 import { Suspense } from 'react';
-import { useLocation } from 'react-router-dom';
 
 import { AppContainer } from './AppContainer.styles';
 
-import Header from '@/components/layout/Header';
 import Router from '@/router';
 
 export default function Layout() {
-  const location = useLocation();
-  const isLandingPage = location.pathname === '/';
-
   return (
     <AppContainer>
       <Suspense>
-        {!isLandingPage && <Header />}
         <Router />
       </Suspense>
     </AppContainer>
