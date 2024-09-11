f125c0e52864997a661f96ecebe24c9382cd8d9b
JUHEEBAEK <bjh9287@naver.com>
2024-09-11 18:51:50 +0900

    Style: image 찌부러지는 것 해결

--- src/components/common/imageBox/Image.styles.ts
+++ src/components/common/imageBox/Image.styles.ts
@@ -5,4 +5,5 @@ import { colors } from '@/styles/colors';
 export const ImageContainer = styled.img`
   border: solid 1px ${colors.border.black};
   border-radius: 0.875rem;
+  object-fit: cover;
 `;
