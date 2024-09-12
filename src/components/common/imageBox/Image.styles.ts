4603ceb6fb10a32f569b669240087d51b971008a
JUHEEBAEK <bjh9287@naver.com>
2024-09-12 23:39:49 +0900

    Fix: image 잘리는 현상 해결

--- src/components/common/imageBox/Image.styles.ts
+++ src/components/common/imageBox/Image.styles.ts
@@ -5,5 +5,4 @@ import { colors } from '@/styles/colors';
 export const ImageContainer = styled.img`
   border: solid 1px ${colors.border.black};
   border-radius: 0.875rem;
-  object-fit: cover;
 `;
