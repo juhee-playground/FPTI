1bee1168bc46a39a547142301526b8f3e9feb96b
JUHEEBAEK <bjh9287@naver.com>
2024-09-18 20:15:04 +0900

    Style: code 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
+// import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global-styles';
 
 import Layout from '@/components/layout';
-
-import './App.css';
 
 function App() {
   return (
