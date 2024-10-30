f856fc57be56f10ca24e0c57ebb91877f33dd1e4
JUHEEBAEK <bjh9287@naver.com>
2024-10-31 00:09:50 +0900

    Chore: next.js 설정 다시 맞추기

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 export default {
-  content: ['./src/**/*.{ts,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        13: 'repeat(13, minmax(0, 1fr))',
+      },
+      colors: {
+        blue: {
+          400: '#2589FE',
+          500: '#0070F3',
+          600: '#2F6FEB',
+        },
+      },
+    },
   },
   plugins: [],
 };
