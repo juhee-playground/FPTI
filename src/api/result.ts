9226d6acb8bc7d060a268a6dd756f959221e3960
JUHEEBAEK <bjh9287@naver.com>
2024-10-30 23:38:30 +0900

    Style: folder 이동

--- src/api/result.ts
+++
@@ -1,5 +0,0 @@
-import dataResult from '@/data/DB_RESULTS.json';
-
-export const findResultById = (fpti: string) => {
-  return dataResult.find(item => item.fpti === fpti) || null;
-};
--- src/components/quiz/QuestionHeader.tsx
+++
@@ -1,9 +0,0 @@
-import { QuestionText } from './Question.styles';
-
-interface IQuestionHeaderProps {
-  questionText: string;
-}
-
-const QuestionHeader = ({ questionText }: IQuestionHeaderProps) => <QuestionText>{questionText}</QuestionText>;
-
-export default QuestionHeader;
