6a60e23b8b00803a74c11d7dc5d471350ea2f363
JUHEEBAEK <bjh9287@naver.com>
2024-11-06 00:37:25 +0900

    Feat: result page 기본 세팅

---
+++ app/result/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+export default function ResultPage() {
+  return (
+    <main className='p-4'>
+      <h1 className='text-3xl font-bold'>Results</h1>
+      <p>Here are your quiz results!</p>
+      {/* Result component goes here */}
+    </main>
+  );
+}
