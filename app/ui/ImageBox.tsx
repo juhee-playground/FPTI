eb551e770b4f9052b966131cfe586e9f9f0e18c6
JUHEEBAEK <bjh9287@naver.com>
2024-11-07 22:03:23 +0900

    Style: component ui 폴더로 이동

---
+++ app/ui/ImageBox.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface IImageBoxProps {
+  topTypes: string | undefined;
+  width: number;
+  height: number;
+}
+
+const ImageBox = ({ topTypes, width, height }: IImageBoxProps) => {
+  const imageSrc = topTypes ? `/images/${topTypes}.webp` : null;
+
+  if (!imageSrc) {
+    return <div>이미지를 찾을 수 없습니다.</div>;
+  }
+
+  return (
+    <img
+      src={imageSrc}
+      alt={`${topTypes} 공룡 이미지`}
+      width={width}
+      height={height}
+      className='border border-black rounded-lg'
+      style={{ width, height }}
+    />
+  );
+};
+
+export default ImageBox;
