15ef22ef97cbf73a510a726fbaae2e811fa99ca5
JUHEEBAEK <bjh9287@naver.com>
2024-11-10 21:29:53 +0900

    Design: text size 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -90,8 +90,9 @@ export default {
         'font-color': 'rgba(0, 0, 0, 1)',
       },
       fontSize: {
-        xs: '0.75rem', // 12px
-        sm: '0.8125rem', // 13px
+        xs: '0.6875rem', // 11px
+        sm: '0.75rem', // 12px
+        md: '0.8125rem', // 13px
         base: '1rem', // 16px
         lg: '1.125rem', // 18px
         xl: '1.25rem', // 20px
