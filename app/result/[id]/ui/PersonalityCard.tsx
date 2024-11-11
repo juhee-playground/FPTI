e8fbe8a6f845b6f4f36317e01d364e1d66f39e98
JUHEEBAEK <bjh9287@naver.com>
2024-11-11 23:13:49 +0900

    Design: 카드 클릭하면 뒤집기 기능추가

--- app/result/[id]/ui/PersonalityCard.tsx
+++ app/result/[id]/ui/PersonalityCard.tsx
@@ -15,18 +15,35 @@ const PersonalityCard = ({ fpti, type, result }: IPersonalityCardProps) => {
   const handleToggleFlip = () => setFlipped(!flipped);
 
   return (
-    <div onClick={handleToggleFlip} className='relative w-96 h-96 perspective cursor-pointer'>
+    <div
+      onClick={handleToggleFlip}
+      className='relative w-96 h-96 perspective cursor-pointer'
+      style={{ perspective: '1000px' }}
+    >
       <div
-        className={`relative w-full h-full transition-transform duration-500 transform-style-preserve-3d ${
-          flipped ? 'rotate-y-180' : ''
-        }`}
+        className={`relative h-full transition-transform duration-500`}
+        style={{
+          transformStyle: 'preserve-3d',
+          transform: flipped ? 'rotateY(180deg)' : 'rotateY(0deg)',
+        }}
       >
         {/* Front Face */}
-        <div className='absolute w-full h-full backface-hidden transform-style-preserve-3d'>
+        <div
+          className='absolute w-full h-full'
+          style={{
+            backfaceVisibility: 'hidden',
+          }}
+        >
           <CardFront fpti={fpti} type={type} result={result} />
         </div>
         {/* Back Face */}
-        <div className='absolute w-full h-full rotate-y-180 backface-hidden transform-style-preserve-3d'>
+        <div
+          className='absolute w-full h-full'
+          style={{
+            transform: 'rotateY(180deg)',
+            backfaceVisibility: 'hidden',
+          }}
+        >
           <CardBack />
         </div>
       </div>
