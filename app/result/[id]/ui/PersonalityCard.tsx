5a7b6a58c53d9db4a2b4e412afb5072b0a933b71
JUHEEBAEK <bjh9287@naver.com>
2024-11-11 22:20:58 +0900

    Feat: hooks useMouse 움직임 추가

--- app/result/[id]/ui/PersonalityCard.tsx
+++ app/result/[id]/ui/PersonalityCard.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import TypeComparisonBar from '@/app/result/[id]/ui/TypeComparisonBar';
-import ImageBox from '@/app/ui/ImageBox';
+import CardBack from './CardBack';
+import CardFront from './CardFront';
 
 interface IPersonalityCardProps {
   fpti: string | undefined;
@@ -10,43 +10,25 @@ interface IPersonalityCardProps {
 }
 
 const PersonalityCard = ({ fpti, type, result }: IPersonalityCardProps) => {
+  const [flipped, setFlipped] = useState(false);
+
+  const handleToggleFlip = () => setFlipped(!flipped);
+
   return (
-    <div className='bg-white max-w-96 w-[90%] p-3 border border-text-placeholder rounded-xl text-text-default'>
-      <div className='flex flex-col items-center bg-background py-3 gap-3 w-full border border-text-placeholder rounded-xl'>
-        <div className='flex justify-center bg-inherit w-full align-middle'>
-          <ImageBox topTypes={fpti as string} width={240} height={240} />
+    <div onClick={handleToggleFlip} className='relative w-96 h-96 perspective cursor-pointer'>
+      <div
+        className={`relative w-full h-full transition-transform duration-500 transform-style-preserve-3d ${
+          flipped ? 'rotate-y-180' : ''
+        }`}
+      >
+        {/* Front Face */}
+        <div className='absolute w-full h-full backface-hidden transform-style-preserve-3d'>
+          <CardFront fpti={fpti} type={type} result={result} />
         </div>
-        <h3 className='flex flex-col items-center gap-1 text-text-basic m-0'>
-          <span className='w-40 h-7 bg-primary text-center rounded-lg font-medium text-lg border border-black'>
-            {fpti}
-          </span>
-          <span className='text-lg'>{type}</span>
-        </h3>
-        {result &&
-          Object.entries(result).map(([group, values]) => {
-            // values가 유효한지 확인
-            if (!values || Object.entries(values).length < 2) {
-              return null; // values가 유효하지 않으면 렌더링하지 않음
-            }
-
-            const [type1, percentage1] = Object.entries(values)[0];
-            const [type2, percentage2] = Object.entries(values)[1];
-
-            const midLabel = values.M ? 'M' : undefined;
-            const isReverse = percentage2 > percentage1;
-            return (
-              <React.Fragment key={group}>
-                <TypeComparisonBar
-                  startLabel={type1}
-                  midLabel={midLabel}
-                  endLabel={type2}
-                  percentage={Math.max(percentage1, percentage2)}
-                  isReverse={isReverse}
-                />
-              </React.Fragment>
-            );
-          })}
-        {/* <p className='text-xs text-text-default text-end mt-1'>FPTI / 나의 풋살타입은? / onthedot</p> */}
+        {/* Back Face */}
+        <div className='absolute w-full h-full rotate-y-180 backface-hidden transform-style-preserve-3d'>
+          <CardBack />
+        </div>
       </div>
     </div>
   );
