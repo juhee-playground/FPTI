8998b9dcb293261e24e587ee72d0bb6981926917
JUHEEBAEK <bjh9287@naver.com>
2024-11-11 22:36:17 +0900

    Chore: storybook 전체 쓰기

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,6 +4,7 @@ import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
   parameters: {
+    layout: 'fullscreen',
     controls: {
       matchers: {
         color: /(background|color)$/i,
