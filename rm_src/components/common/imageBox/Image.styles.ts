ddc15d2fc4a3891053f3af407b0fbabd275190a5
JUHEEBAEK <bjh9287@naver.com>
2024-11-11 20:55:02 +0900

    Style: 공통 컴포넌트도 필요없는 파일 지우기

--- rm_src/components/common/imageBox/Image.styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-import { colors } from '@/styles/colors';
-
-export const ImageContainer = styled.img`
-  border: solid 1px ${colors.border.black};
-  border-radius: 0.875rem;
-`;
