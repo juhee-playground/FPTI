aaa524e5030aaf6b2257738faf6093b14e940648
JUHEEBAEK <bjh9287@naver.com>
2024-11-11 22:20:43 +0900

    Feat: card 뒷면 추가

---
+++ app/result/[id]/ui/CardBack.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CardBack = () => {
+  return <img src='/bg-back.png' alt='카드 뒷면' className='w-full h-full object-contain rounded-xl' />;
+};
+
+export default CardBack;
