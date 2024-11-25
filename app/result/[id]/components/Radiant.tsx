055fa49094b4865912fe666ca6c1f9ef8850f247
JUHEEBAEK <bjh9287@naver.com>
2024-11-25 11:00:59 +0900

    Fix: Radinat도 마우스 포인터에 따라 변하도록 수정

--- app/result/[id]/components/Radiant.tsx
+++ app/result/[id]/components/Radiant.tsx
@@ -1,5 +1,31 @@
+import React, { ReactNode } from 'react';
+
 import styles from './Radiant.module.css';
 
-const Radiant: React.FC = () => <div className={styles.radiant} />;
+import useInteract from '@/hooks/useInteract';
+
+interface IHoloProps {
+  children: ReactNode;
+  dynamicStylesProps?: React.CSSProperties;
+}
+
+const Radiant = ({ children, dynamicStylesProps }: IHoloProps) => {
+  const { handleMove, handleLeave, dynamicStyles } = useInteract();
+  const appliedStyles = dynamicStylesProps || dynamicStyles;
+
+  console.log(appliedStyles);
+
+  return (
+    <div
+      className={styles.container}
+      onMouseMove={handleMove}
+      onMouseLeave={handleLeave}
+      style={appliedStyles as React.CSSProperties}
+    >
+      <div className={styles.radiant} />
+      {children}
+    </div>
+  );
+};
 
 export default Radiant;
