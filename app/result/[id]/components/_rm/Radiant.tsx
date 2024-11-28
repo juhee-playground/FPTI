997511fdb6b7030651208b1d9a0a8fd10440c7e7
JUHEEBAEK <bjh9287@naver.com>
2024-11-28 23:46:20 +0900

    Style: Radiant 컴포넌트로 쪼개기

---
+++ app/result/[id]/components/_rm/Radiant.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode } from 'react';
+
+import styles from './RadiantHolo.module.css';
+
+import useInteract from '@/hooks/useInteract';
+
+interface IRadiantProps {
+  children: ReactNode;
+  dynamicStylesProps?: React.CSSProperties;
+}
+
+const Radiant = ({ children, dynamicStylesProps }: IRadiantProps) => {
+  const { handleMove, handleLeave, dynamicStyles } = useInteract();
+  const appliedStyles = dynamicStylesProps || (dynamicStyles as React.CSSProperties);
+
+  return (
+    <div
+      className={styles.container}
+      onMouseMove={handleMove}
+      onMouseLeave={handleLeave}
+      style={appliedStyles as React.CSSProperties}
+    >
+      <div className={styles.radiant} />
+      {children}
+    </div>
+  );
+};
+
+export default Radiant;
