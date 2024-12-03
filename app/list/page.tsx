4d6bd30dec05e86714643d455877098bda937169
JUHEEBAEK <bjh9287@naver.com>
2024-12-03 23:53:01 +0900

    Feat: list 페이지 생성

---
+++ app/list/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function ListPage() {
+  return (
+    <main className='mx-auto shadow-md shadow-black/10 flex flex-col relative'>
+      <h1>Card List</h1>
+    </main>
+  );
+}
