97d5e15560a22b06c940f24e389f91e4530b1a3b
JUHEEBAEK <bjh9287@naver.com>
2024-12-06 19:30:51 +0900

    Feat: card List 보여주도록 수정

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,9 +1,46 @@
 'use client';
 
+import React from 'react';
+
+import PersonalityCard from '../result/[id]/ui/PersonalityCard';
+
+import dataResult from '@/data/DB_RESULTS.json';
+
 export default function ListPage() {
+  const cards = dataResult;
+
+  const generateResult = (card: IDinosaurCard) => {
+    const fpti = card.fpti;
+
+    return {
+      '책임감과 주도성': {
+        L: fpti[0] === 'L' ? 100 : 0,
+        S: fpti[0] === 'S' ? 100 : 0,
+      },
+      '팀에서의 역할': {
+        A: fpti[1] === 'A' ? 100 : 0,
+        D: fpti[1] === 'D' ? 100 : 0,
+      },
+      '플레이 스타일': {
+        T: fpti[2] === 'T' ? 100 : 0,
+        P: fpti[2] === 'P' ? 100 : 0,
+      },
+      '목표와 우선순위': {
+        C: fpti[3] === 'C' ? 100 : 0,
+        E: fpti[3] === 'E' ? 100 : 0,
+      },
+    };
+  };
+
   return (
-    <main className='mx-auto shadow-md shadow-black/10 flex flex-col relative'>
+    <main className='p-4 mx-auto max-w-full'>
       <h1>Card List</h1>
+      <div className='mx-auto flex flex-wrap gap-20'>
+        {cards.map(card => {
+          const result = generateResult(card);
+          return <PersonalityCard key={card.id} fpti={card.fpti} type={card.type} result={result} />;
+        })}
+      </div>
     </main>
   );
 }
