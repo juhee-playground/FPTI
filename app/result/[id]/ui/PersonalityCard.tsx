18d72da7093576f4bcde83dfc35ae6778b7650db
JUHEEBAEK <bjh9287@naver.com>
2024-12-19 10:22:18 +0900

    Feat: Glare 효과 추가

--- app/result/[id]/ui/PersonalityCard.tsx
+++ app/result/[id]/ui/PersonalityCard.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import CardFront from './CardFront';
 
+import Glare from '@/app/result/[id]/ui/animation/Glare';
 import Rotator from '@/app/result/[id]/ui/animation/Rotator';
 import Shine from '@/app/result/[id]/ui/animation/Shine';
 import useInteract from '@/hooks/useInteract';
@@ -36,19 +37,21 @@ const PersonalityCard = ({ fpti, type, result }: IPersonalityCardProps) => {
     <div onClick={handleToggleFlip} className='card-container' onMouseMove={handleMove} onMouseLeave={handleLeave}>
       <Rotator rotationProps={rotation} isFlipped={flipped}>
         <Shine dynamicStylesProps={dynamicStyles}>
-          <div className='card__back' style={rotateStyleB}>
-            <img
-              width={320}
-              height={491}
-              src='/bg-back.png'
-              alt='카드 뒷면'
-              className='border border-text-placeholder rounded-xl'
-            />
-          </div>
+          <Glare dynamicStylesProps={dynamicStyles}>
+            <div className='card__back' style={rotateStyleB}>
+              <img
+                width={320}
+                height={491}
+                src='/bg-back.png'
+                alt='카드 뒷면'
+                className='border border-text-placeholder rounded-xl'
+              />
+            </div>
 
-          <div className='card__front' style={rotateStyleF}>
-            <CardFront fpti={fpti} type={type} result={result} />
-          </div>
+            <div className='card__front' style={rotateStyleF}>
+              <CardFront fpti={fpti} type={type} result={result} />
+            </div>
+          </Glare>
         </Shine>
       </Rotator>
     </div>
