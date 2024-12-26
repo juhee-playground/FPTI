d9aba55c1bff239d3248eb8e62310a1495005055
JUHEEBAEK <bjh9287@naver.com>
2024-12-26 16:07:57 +0900

    Feat: RadinatHolo animation Card 추가

---
+++ app/result/[id]/ui/RadiantHoloCard.stories.tsx
@@ -0,0 +1,39 @@
+import RadiantHoloCard from './RadiantHoloCard';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'RadiantHoloCard----',
+  component: RadiantHoloCard,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof RadiantHoloCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Card: Story = {
+  args: {
+    fpti: 'LATE',
+    type: '쇼맨의 리더형',
+    result: {
+      '책임감과 주도성': {
+        L: 57,
+        S: 43,
+      },
+      '팀에서의 역할': {
+        A: 100,
+        D: 0,
+      },
+      '플레이 스타일': {
+        T: 52,
+        P: 48,
+      },
+      '목표와 우선순위': {
+        C: 40,
+        E: 60,
+      },
+    },
+  },
+};
---
+++ app/result/[id]/ui/RadiantHoloCard.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+import CardFront from './CardFront';
+
+// import Glare from '@/app/result/[id]/ui/animation/Glare';
+import RadiantHoloPattern from '@/app/result/[id]/ui/animation/RadiantHoloPattern';
+import Rotator from '@/app/result/[id]/ui/animation/Rotator';
+import Shine from '@/app/result/[id]/ui/animation/Shine';
+import useInteract from '@/hooks/useInteract';
+
+interface IPersonalityCardProps {
+  fpti: string;
+  type: string | undefined;
+  result: { [group: string]: IScaleValue };
+}
+
+const RadiantHoloCard = ({ fpti, type, result }: IPersonalityCardProps) => {
+  const [flipped, setFlipped] = useState(false);
+  const { rotation, transition, dynamicStyles, handleMove, handleLeave } = useInteract();
+
+  const handleToggleFlip = () => setFlipped(!flipped);
+
+  const rotateStyleF = {
+    transform: `perspective(1000px) ${flipped ? 'rotateY(180deg)' : ''}`,
+    transition,
+  };
+
+  const rotateStyleB = {
+    transform: `perspective(1000px) ${flipped ? 'rotateY(0deg)' : ''}`,
+    transition,
+  };
+
+  return (
+    <div onClick={handleToggleFlip} className='card-container' onMouseMove={handleMove} onMouseLeave={handleLeave}>
+      <Rotator rotationProps={rotation}>
+        <RadiantHoloPattern dynamicStylesProps={dynamicStyles} radiant={true} holo={false} fpti={fpti}>
+          <Shine dynamicStylesProps={dynamicStyles}>
+            <div className='card__back' style={rotateStyleB}>
+              <img
+                width={320}
+                height={491}
+                src='/bg-back.png'
+                alt='카드 뒷면'
+                className='border border-text-placeholder rounded-xl'
+              />
+            </div>
+
+            <div className='card__front' style={rotateStyleF}>
+              <CardFront fpti={fpti} type={type} result={result} />
+            </div>
+          </Shine>
+        </RadiantHoloPattern>
+      </Rotator>
+    </div>
+  );
+};
+
+export default RadiantHoloCard;
