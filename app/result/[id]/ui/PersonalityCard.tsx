3916bd923b925252f5c0fd458edbd83186eb4dc6
JUHEEBAEK <bjh9287@naver.com>
2025-01-11 17:33:30 +0900

    Style: global.css -> module.css로 쓰도록 사용방법 수정

--- app/result/[id]/ui/PersonalityCard.tsx
+++ app/result/[id]/ui/PersonalityCard.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+import styles from './Card.module.css';
 import CardFront from './CardFront';
 
 import Glare from '@/app/result/[id]/ui/animation/Glare';
@@ -30,22 +31,29 @@ const PersonalityCard = ({ fpti, type, result }: IPersonalityCardProps) => {
   };
 
   return (
-    <div onClick={handleToggleFlip} className='card-container' onMouseMove={handleMove} onMouseLeave={handleLeave}>
+    <div
+      className={styles['card-container']}
+      onClick={handleToggleFlip}
+      onMouseMove={handleMove}
+      onMouseLeave={handleLeave}
+    >
       <Rotator rotationProps={rotation}>
         <Shine dynamicStylesProps={dynamicStyles}>
           <Glare dynamicStylesProps={dynamicStyles}>
-            <div className='card__back' style={rotateStyleB}>
-              <img
-                width={320}
-                height={491}
-                src='/bg-back.png'
-                alt='카드 뒷면'
-                className='border border-text-placeholder rounded-xl'
-              />
-            </div>
+            <div className={styles['card']}>
+              <div className={styles['card__back']} style={rotateStyleB}>
+                <img
+                  width={320}
+                  height={491}
+                  src='/bg-back.png'
+                  alt='카드 뒷면'
+                  className='border border-text-placeholder rounded-xl'
+                />
+              </div>
 
-            <div className='card__front' style={rotateStyleF}>
-              <CardFront fpti={fpti} type={type} result={result} />
+              <div className={styles['card__front']} style={rotateStyleF}>
+                <CardFront fpti={fpti} type={type} result={result} />
+              </div>
             </div>
           </Glare>
         </Shine>
