d7749dcbdbd14ea6e35e743d2c82b25939b96f19
JUHEEBAEK <bjh9287@naver.com>
2025-01-11 19:19:58 +0900

    Chore: 외부 image url 설정에 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['juhee100bucket.s3.ap-northeast-2.amazonaws.com'], // 외부 이미지 호스트 추가
+  },
+};
 
 export default nextConfig;
